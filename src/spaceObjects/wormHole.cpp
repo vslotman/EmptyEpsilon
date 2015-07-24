9758b696fc102f689e87e58cc88db49e86941509
vslotman <vslotman@gmail.com>
2015-07-24 12:01:37 +0200

    Added randomness to target. Started with nebula-like appearance

--- src/spaceObjects/wormHole.cpp
+++ src/spaceObjects/wormHole.cpp
@@ -1,3 +1,5 @@
+#include <SFML/OpenGL.hpp>
+
 #include "main.h"
 #include "wormHole.h"
 #include "spaceship.h"
@@ -8,6 +10,7 @@
 #define ALPHA_MULTIPLIER          10.0
 #define DEFAULT_COLLISION_RADIUS  5000
 #define AVOIDANCE_MULTIPLIER      1.2
+#define TARGET_SPREAD             500
 
 /// A Wormhole object. Drags you in like a black hole and teleports when you're at the center.
 REGISTER_SCRIPT_SUBCLASS(WormHole, SpaceObject)
@@ -16,11 +19,54 @@ REGISTER_SCRIPT_SUBCLASS(WormHole, SpaceObject)
 
 REGISTER_MULTIPLAYER_CLASS(WormHole, "WormHole");
 WormHole::WormHole()
-: SpaceObject(DEFAULT_COLLISION_RADIUS, "WormHole")
+: SpaceObject(DEFAULT_COLLISION_RADIUS, "WormHole") 
 {
     pathPlanner = PathPlannerManager::getInstance();
     pathPlanner->addAvoidObject(this, (DEFAULT_COLLISION_RADIUS * AVOIDANCE_MULTIPLIER) );
+    
+    // Create some overlaying clouds
+    for(int n=0; n<cloud_count; n++)
+    {
+        clouds[n].size = random(1024, 1024 * 4);
+        clouds[n].texture = irandom(1, 3);
+        clouds[n].offset = sf::Vector2f(0, 0); 
+    }
 }
+
+#if FEATURE_3D_RENDERING
+void WormHole::draw3DTransparent()
+{
+    glRotatef(getRotation(), 0, 0, -1);
+    glTranslatef(-getPosition().x, -getPosition().y, 0);
+    for(int n=0; n<cloud_count; n++)
+    {
+        NebulaCloud& cloud = clouds[n];
+
+        sf::Vector3f position = sf::Vector3f(getPosition().x, getPosition().y, 0) + sf::Vector3f(cloud.offset.x, cloud.offset.y, 0);
+        float size = cloud.size;
+        
+        float distance = sf::length(camera_position - position);
+        float alpha = 1.0 - (distance / 10000.0f);
+        if (alpha < 0.0)
+            continue;
+
+        billboardShader->setParameter("textureMap", *textureManager.getTexture("Nebula" + string(cloud.texture) + ".png"));
+        sf::Shader::bind(billboardShader);
+        glBegin(GL_QUADS);
+        glColor4f(alpha * 0.8, alpha * 0.8, alpha * 0.8, size);
+        glTexCoord2f(0, 0);
+        glVertex3f(position.x, position.y, position.z);
+        glTexCoord2f(1, 0);
+        glVertex3f(position.x, position.y, position.z);
+        glTexCoord2f(1, 1);
+        glVertex3f(position.x, position.y, position.z);
+        glTexCoord2f(0, 1);
+        glVertex3f(position.x, position.y, position.z);
+        glEnd();
+    }
+}
+#endif//FEATURE_3D_RENDERING
+
 
 void WormHole::drawOnRadar(sf::RenderTarget& window, sf::Vector2f position, float scale, bool long_range)
 {
@@ -32,6 +78,7 @@ void WormHole::drawOnRadar(sf::RenderTarget& window, sf::Vector2f position, floa
     window.draw(objectSprite);
 }
 
+/* Draw a line towards the target position */
 void WormHole::drawOnGMRadar(sf::RenderTarget& window, sf::Vector2f position, float scale, bool long_range)
 {    
     sf::VertexArray a(sf::Lines, 2);
@@ -50,23 +97,28 @@ void WormHole::update(float delta)
 void WormHole::collide(Collisionable* target)
 {
     if (update_delta == 0.0)
-    return;
+        return;
 
     sf::Vector2f diff = getPosition() - target->getPosition();
     float distance = sf::length(diff);
     float force = (getRadius() * getRadius() * FORCE_MULTIPLIER) / (distance * distance);
+    
     P<SpaceShip> obj = P<Collisionable>(target);
     
     if (force > FORCE_MAX)
     {
         force = FORCE_MAX;
         if (isServer())
-            target->setPosition(target_position);
-            obj->wormhole_alpha = 0.0;
+            target->setPosition( (target_position + 
+                                  sf::Vector2f(random(-TARGET_SPREAD, TARGET_SPREAD), 
+                                               random(-TARGET_SPREAD, TARGET_SPREAD))));
+            if (obj)
+                obj->wormhole_alpha = 0.0;
     }
     
     // warp postprocessor-alpha is calculated using alpha = (1 - (delay/10))
-    obj->wormhole_alpha = ((distance / getRadius()) * ALPHA_MULTIPLIER);
+    if (obj)
+        obj->wormhole_alpha = ((distance / getRadius()) * ALPHA_MULTIPLIER);
     
     //TODO Change setPosition to something Newtonianish. Now escaping is impossible
     target->setPosition(target->getPosition() + diff / distance * update_delta * force);
