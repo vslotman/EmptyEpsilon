ec8c5ae4c0db5c53c80537209aafa53e0f175cb6
vslotman <vslotman@gmail.com>
2015-08-30 14:52:22 +0200

    engineering slider without mouse

--- src/gui/gui2_slider.cpp
+++ src/gui/gui2_slider.cpp
@@ -166,3 +166,8 @@ float GuiSlider::getValue()
 {
     return value;
 }
+
+GuiSliderWithoutMouse::GuiSliderWithoutMouse(GuiContainer* owner, string id, float min_value, float max_value, float start_value, func_t func)
+: GuiSlider(owner, id, min_value, max_value, start_value, func)
+{
+}
