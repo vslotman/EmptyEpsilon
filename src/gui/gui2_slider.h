71f55122ab878527fc5842aa86e13a2e86f6ca0f
vslotman <vslotman@gmail.com>
2015-08-30 14:52:22 +0200

    engineering slider without mouse

--- src/gui/gui2_slider.h
+++ src/gui/gui2_slider.h
@@ -32,4 +32,13 @@ public:
     float getValue();
 };
 
+class GuiSliderWithoutMouse : public GuiSlider
+{
+public:
+    GuiSliderWithoutMouse(GuiContainer* owner, string id, float min_value, float max_value, float start_value, func_t func);
+    virtual bool onMouseDown(sf::Vector2f position) { return false; }
+    virtual void onMouseDrag(sf::Vector2f position) {}
+    virtual void onMouseUp(sf::Vector2f position)   {}
+};
+
 #endif//GUI2_SLIDER_H
